046daec61abeeee7fed80318c92d833f0bcf404d
bhargavthanth001 <bhargavthanth69@gmail.com>
2023-12-26 18:46:56 +0530

    Day 8 completed

--- lib/buyer_module/upload_nft/upload_nfts.dart
+++ lib/buyer_module/upload_nft/upload_nfts.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
+import 'package:audio_service/audio_service.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:easy_audio_player/widgets/players/basic_audio_player.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:gallery_picker/gallery_picker.dart';
 import 'package:gap/gap.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:nft_marketplace/colors.dart';
+import 'package:vimeo_video_player_custom/vimeo_video_player_custom.dart';
 
 class UploadNftPageWidget extends StatefulWidget {
   final String type;
@@ -14,7 +20,9 @@ class UploadNftPageWidget extends StatefulWidget {
 }
 
 class _UploadNftPageWidgetState extends State<UploadNftPageWidget> {
-  List<MediaFile>? mediaFile;
+  File? thumbnailImage;
+  File? audioFile;
+  File? file;
 
   @override
   Widget build(BuildContext context) {
@@ -27,90 +35,137 @@ class _UploadNftPageWidgetState extends State<UploadNftPageWidget> {
             ? Column(
                 children: [
                   const Gap(20),
-                  SizedBox(
-                      height: 150,
-                      width: 300,
-                      child: DottedBorder(
-                        color: ColorsData.selectiveYellow,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              MaterialButton(
-                                  height: 30,
-                                  minWidth: 40,
-                                  shape: RoundedRectangleBorder(
-                                      side: const BorderSide(
-                                        color: Colors.blueAccent,
-                                      ),
-                                      borderRadius: BorderRadius.circular(30)),
-                                  onPressed: () {},
-                                  child: const Text("choose Audio")),
-                              const Gap(8),
-                              const Text(
-                                "Upload MP3, FLAC",
-                                style: TextStyle(color: Colors.black45),
-                              ),
-                              const Gap(5),
-                              const Text(
-                                "File size - Max 20mb",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
+                  audioFile == null
+                      ? SizedBox(
+                          height: 150,
+                          width: 300,
+                          child: DottedBorder(
+                            color: ColorsData.selectiveYellow,
+                            child: Center(
+                                child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                MaterialButton(
+                                    height: 30,
+                                    minWidth: 40,
+                                    shape: RoundedRectangleBorder(
+                                        side: const BorderSide(
+                                          color: Colors.blueAccent,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(30)),
+                                    onPressed: () async {
+                                      pickAudio();
+                                    },
+                                    child: const Text("choose Audio")),
+                                const Gap(8),
+                                const Text(
+                                  "Upload MP3, FLAC",
+                                  style: TextStyle(color: Colors.black45),
                                 ),
-                              ),
-                            ],
+                                const Gap(5),
+                                const Text(
+                                  "File size - Max 20mb",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            )),
+                          ),
+                        )
+                      : Container(
+                          height: 135,
+                          width: 300,
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: ColorsData.selectiveYellow,
+                          ),
+                          child: BasicAudioPlayer(
+                            autoPlay: false,
+                            playlist: ConcatenatingAudioSource(
+                              children: [
+                                AudioSource.uri(
+                                  Uri.parse(
+                                    audioFile!.path,
+                                  ),
+                                  tag: thumbnailImage != null
+                                      ? MediaItem(
+                                          id: '1',
+                                          artUri: Uri.parse(
+                                            thumbnailImage!.path,
+                                          ),
+                                          title: 'Audio Title ',
+                                          album: 'amazing album')
+                                      : MediaItem(
+                                          id: '1',
+                                          artUri: Uri.parse(
+                                              "assets/images/logo.jpg"),
+                                          title: 'Audio Title ',
+                                          album: 'amazing album'),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      )),
                   const Gap(15),
                   SizedBox(
-                      height: 300,
-                      width: 300,
-                      child: DottedBorder(
-                        color: ColorsData.selectiveYellow,
-                        child: Center(
-                            child: mediaFile == null
-                                ? Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      MaterialButton(
-                                          height: 30,
-                                          minWidth: 40,
-                                          shape: RoundedRectangleBorder(
-                                              side: const BorderSide(
-                                                color: Colors.blueAccent,
-                                              ),
-                                              borderRadius:
-                                                  BorderRadius.circular(30)),
-                                          onPressed: () async {
-                                            mediaFile =
-                                                await GalleryPicker.pickMedia(
-                                                    context: context,
-                                                    singleMedia: true);
-                                          },
-                                          child:
-                                              const Text("choose Thumbnail")),
-                                      const Gap(8),
-                                      const Text(
-                                        "Upload PNG,GIF,JPG,JPEG",
-                                        style: TextStyle(color: Colors.black45),
-                                      ),
-                                      const Gap(5),
-                                      const Text(
-                                        "File size - Max 25mb",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                : const Padding(
-                                    padding: EdgeInsets.all(5),
-                                    // child: Image.file(),
-                                  )),
-                      ))
+                    height: 300,
+                    width: 300,
+                    child: DottedBorder(
+                      color: ColorsData.selectiveYellow,
+                      child: Center(
+                        child: thumbnailImage == null
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  MaterialButton(
+                                      height: 30,
+                                      minWidth: 40,
+                                      shape: RoundedRectangleBorder(
+                                          side: const BorderSide(
+                                            color: Colors.blueAccent,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(30)),
+                                      onPressed: () async {
+                                        pickProfileFromGallary();
+                                      },
+                                      child: const Text("choose Thumbnail")),
+                                  const Gap(8),
+                                  const Text(
+                                    "Upload PNG,GIF,JPG,JPEG",
+                                    style: TextStyle(color: Colors.black45),
+                                  ),
+                                  const Gap(5),
+                                  const Text(
+                                    "File size - Max 25mb",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            : Container(
+                                height: 300,
+                                width: 300,
+                                margin: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  image: DecorationImage(
+                                    image: FileImage(
+                                      File(thumbnailImage!.path),
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                      ),
+                    ),
+                  )
                 ],
               )
             : SizedBox(
@@ -119,44 +174,107 @@ class _UploadNftPageWidgetState extends State<UploadNftPageWidget> {
                 child: DottedBorder(
                   color: ColorsData.selectiveYellow,
                   child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        MaterialButton(
-                            height: 30,
-                            minWidth: 40,
-                            shape: RoundedRectangleBorder(
-                                side: const BorderSide(
-                                  color: Colors.blueAccent,
+                      child: file == null
+                          ? Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                MaterialButton(
+                                    height: 30,
+                                    minWidth: 40,
+                                    shape: RoundedRectangleBorder(
+                                        side: const BorderSide(
+                                          color: Colors.blueAccent,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(30)),
+                                    onPressed: () {
+                                      if (widget.type == "video") {
+                                        pickVideo();
+                                      } else {
+                                        pickProfileFromGallary();
+                                      }
+                                    },
+                                    child: widget.type == "image"
+                                        ? const Text("choose image")
+                                        : const Text("choose video")),
+                                const Gap(8),
+                                Text(
+                                  widget.type == "image"
+                                      ? "Upload PNG,GIF,JPG,JPEG"
+                                      : "Upload MP4, WAV",
+                                  style: const TextStyle(color: Colors.black45),
                                 ),
-                                borderRadius: BorderRadius.circular(30)),
-                            onPressed: () {},
-                            child: widget.type == "image"
-                                ? const Text("choose image")
-                                : const Text("choose video")),
-                        const Gap(8),
-                        Text(
-                          widget.type == "image"
-                              ? "Upload PNG,GIF,JPG,JPEG"
-                              : "Upload MP4, WAV",
-                          style: const TextStyle(color: Colors.black45),
-                        ),
-                        const Gap(5),
-                        Text(
-                          widget.type == "image"
-                              ? "File size - Max 25mb"
-                              : "File size - Max 100mb",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                                const Gap(5),
+                                Text(
+                                  widget.type == "image"
+                                      ? "File size - Max 25mb"
+                                      : "File size - Max 100mb",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            )
+                          : Container(
+                              height: 300,
+                              width: 300,
+                              padding: const EdgeInsets.all(10),
+                              child: widget.type == "image"
+                                  ? Image.file(
+                                      File(
+                                        file!.path,
+                                      ),
+                                      fit: BoxFit.cover,
+                                    )
+                                  : VimeoVideoPlayer(
+                                      url: file!.path,
+                                      // onFinished: () => onFinishedVimeo(),
+                                    ),
+                            )),
                 ),
               ),
       ),
     );
+  }
+
+  Future pickProfileFromGallary() async {
+    final thumbnailImagePicker = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: [
+        "PNG",
+        "GIF",
+        "JPG",
+        "JPEG",
+      ],
+    );
+    debugPrint('image path');
+    setState(() {
+      file = File(thumbnailImagePicker!.files.single.path!);
+    });
+  }
+
+  Future pickAudio() async {
+    final thumbnailImagePicker = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: [
+      "MP3",
+      "FLAC",
+    ]);
+    debugPrint('image path');
+    setState(() {
+      audioFile = File(thumbnailImagePicker!.files.single.path!);
+    });
+  }
+
+  Future pickVideo() async {
+    final thumbnailImagePicker = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: [
+      "MP4",
+      "WAV",
+    ]);
+    debugPrint('image path');
+    setState(() {
+      file = File(thumbnailImagePicker!.files.single.path!);
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,26 +5,34 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
+import audio_session
 import cloud_firestore
 import connectivity_plus
-import device_info_plus
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 import video_player_avfoundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
